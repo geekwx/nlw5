9d9e7e23f331e9fcf4f5c91d884b514dc44ae4b2
Wesley Roberto <suporte@ubbonline.org.br>
2021-04-19 12:04:39 -0300

    commit initial

---
+++ backend_node/src/server.ts
@@ -0,0 +1,15 @@
+import express, { response } from "express";
+const app = express();
+
+
+
+
+app.get("/" , (request, response) => {
+    return response.send("Ola NLW 05");
+})
+
+app.post("/", (request, response) => {
+    return response.json({ message: "Usuario salvo com sucesso!"})
+});
+
+app.listen(3333, () => console.log("Server is running on port 3333"))
