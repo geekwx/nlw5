dfb4505ff36bbe655cc712d167c095f7def9ce33
Wesley Roberto <suporte@ubbonline.org.br>
2021-04-20 17:49:59 -0300

    aula2

--- backend_node/src/server.ts
+++ backend_node/src/server.ts
@@ -1,15 +1,18 @@
-import express, { response } from "express";
+import express from "express";
+
+
+import "./database"
+
+import { routes } from './routes' ;
+
+
 const app = express();
+app.use(express.json())
+
+app.use(routes);
 
 
 
 
-app.get("/" , (request, response) => {
-    return response.send("Ola NLW 05");
-})
-
-app.post("/", (request, response) => {
-    return response.json({ message: "Usuario salvo com sucesso!"})
-});
 
 app.listen(3333, () => console.log("Server is running on port 3333"))
